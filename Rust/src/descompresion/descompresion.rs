f6808e1a261ab19baadc38c31ece55ff9cec72ef
Agustin <aguscambiano@gmail.com>
2020-12-14 21:02:48 -0300

    Agrego chequeo de argumentos del programa

--- Rust/src/descompresion/descompresion.rs
+++ Rust/src/descompresion/descompresion.rs
@@ -1,16 +1,14 @@
 use std::fs;
 use std::env;
 
+const FILE_INDEX: usize = 2;
+const ARGS_AMOUNT: usize = 3;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut i = 0;
-    for arg in args.iter() {
-        println!("Argumento {0}: {1}", i, arg);
-        i += 1;
+    if (args.len() != ARGS_AMOUNT) {
+        panic!("Incorrect amount of arguments");
     }
-
-    let compressed_file = fs::read_to_string(filename).expect("Something went wrong \
-                                                                     reading the file");
-
+    let compressed_file = fs::read_to_string(&args[FILE_INDEX]).expect("Inexistent file");
 
 }
