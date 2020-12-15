2f63694c00d4781dd7edda79c9e5864285c9efd5
Agustin <aguscambiano@gmail.com>
2020-12-15 17:30:34 -0300

    Agrego decompresion de un caracter

--- Rust/src/descompresion/descompresion.rs
+++ Rust/src/descompresion/descompresion.rs
@@ -1,18 +1,21 @@
 use std::fs;
 use std::env;
+//use crate::huffman::huffman_tree::HuffmanTree;
+
+//mod huffman::huffman_tree;
 
 const FILE_INDEX: usize = 2;
 const ARGS_AMOUNT: usize = 3;
-const BITS_PER_BYTE: usize = 8;
+const BITS_PER_BYTE: u8 = 8;
 
 //Returns the number of read bits
-fn get_char(bytes: &Vec<u8>, byte_to_read: &mut usize, read_bits: usize, decompressed_file: &mut String) -> u8 {
+fn get_char(bytes: &Vec<u8>, byte_to_read: &mut usize, read_bits: u8, decompressed_file: &mut String) -> u8 {
     //Leaves the already read bits in 0
     //let mut aux_byte: u8 = (bytes[byte_to_read] << read_bits) >> read_bits;
 
-    let was_letter_decoded = false;
+    let mut was_letter_decoded = false;
     let mut tree_code: String;
-    let mut _read_bits = read_bits;
+    let mut _read_bits: u8 = read_bits;
 
     while !was_letter_decoded {
         //Gets the corresponding bit
@@ -20,11 +23,18 @@ fn get_char(bytes: &Vec<u8>, byte_to_read: &mut usize, read_bits: usize, decompr
 
         tree_code.push(std::char::from_u32(aux_byte).expect("Invalid number for tree code"));
         _read_bits += 1;
-        
+        match HuffmanTree::decode(&tree_code) {
+            Ok(letter) => {
+                decompressed_file.push(letter);
+                was_letter_decoded = True;
+            },
+        }
+        if _read_bits == BITS_PER_BYTE {
+            _read_bits = 0;
+            (*byte_to_read) += 1;
+        }
     }
-
-    //CAMBIAR EL RETURN
-    return 0;
+    return _read_bits;
 }
 
 
