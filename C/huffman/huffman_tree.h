2cd3f9f95a266fbf93616427a7fd222c86b1294d
Agustin <aguscambiano@gmail.com>
2020-12-17 18:10:36 -0300

    Cambio el huffman_tree para que se pueda guardar en el stack

--- C/huffman/huffman_tree.h
+++ C/huffman/huffman_tree.h
@@ -1,13 +1,23 @@
 #ifndef C_HUFFMAN_TREE_H
 #define C_HUFFMAN_TREE_H
 
-struct huffman_tree;
+#include "huffman_node.h"
 
-typedef struct huffman_tree huffman_tree_t;
+//struct huffman_tree;
+//typedef struct huffman_tree huffman_tree_t;
 
-huffman_tree_t *huffman_tree_create();
+typedef struct huffman_tree {
+    huffman_node_t *root;
+}huffman_tree_t;
 
-void huffman_tree_destroy(huffman_tree_t *tree);
+
+//huffman_tree_t *huffman_tree_create();
+
+//void huffman_tree_destroy(huffman_tree_t *tree);
+
+int huffman_tree_init(huffman_tree_t *tree);
+
+void huffman_tree_release(huffman_tree_t *tree);
 
 void huffman_tree_print(huffman_tree_t *tree);
 
