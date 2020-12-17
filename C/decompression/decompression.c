ba5dccc2cd532566f87ff1706b35aa0b685f98d4
Agustin <aguscambiano@gmail.com>
2020-12-17 17:11:43 -0300

    Sigo traduccion a C

--- C/decompression/decompression.c
+++ C/decompression/decompression.c
@@ -8,6 +8,8 @@
 
 #define COMPRESSED_EXTENSION ".huffman"
 #define DECOMPRESSED_EXTENSION ".txt"
+#define BITS_PER_BYTE 8
+
 #define SUCCESS 0
 #define BAD_FILE_NAME -1
 #define MEMORY_ERROR -2
@@ -37,7 +39,7 @@ bool is_file_name_valid(const char* file_name) {
     return (extension_ptr - file_name) + strlen(extension_ptr) == strlen(file_name);
 }
 
-int load_file(const char* file_name, char** compressed_file_string) {
+int load_file(const char* file_name, char** compressed_file_string, int* file_len) {
     FILE* compressed_file = fopen(file_name, "r");
     if (!compressed_file) {
         return FILE_ERROR;
@@ -61,23 +63,64 @@ int load_file(const char* file_name, char** compressed_file_string) {
     return SUCCESS;
 }
 
+//Returns the number of read bits
+fn get_char(bytes: &Vec<u8>, huff_tree: &HuffmanCompression, byte_to_read: &mut usize, read_bits: u8,
+decompressed_file: &mut String) -> u8 {
+let mut aux_byte: u8;
+let mut was_letter_decoded = false;
+let mut tree_code: String = String::from("");
+let mut _read_bits: u8 = read_bits;
 
-int execute_decompression(const char* file_name, const char* compressed_file, ) {
+while !was_letter_decoded {
+//Gets the corresponding bit
+aux_byte = (bytes[*byte_to_read] << read_bits) >> (read_bits + BITS_PER_BYTE - (read_bits + 1));
+
+tree_code.push(std::char::from_u32(aux_byte as u32).expect("Invalid number for tree code"));
+_read_bits += 1;
+match huff_tree.decode(&tree_code) {
+Some(letter) => {
+decompressed_file.push(letter);
+was_letter_decoded = true;
+},
+None => {},
+}
+if _read_bits == BITS_PER_BYTE {
+_read_bits = 0;
+(*byte_to_read) += 1;
+}
+}
+return _read_bits;
+}
+
+char get_char(const char* compressed_file_string, huff_tree: &HuffmanCompression, int* byte_to_read, char read_bits,
+              bytes_vector_t* decompressed_file) -> u8 {
+
+}
+int execute_decompression(const char* file_name, const char* compressed_file_string, int compressed_file_len) {
     char padding_bits = compressed_file_string[0];
     int byte_to_read = 1;
     int read_bits = 0;
+    char last_byte_read_bits = 0;
+
+    //CREAR EL HUFF_TREE
 
     bytes_vector_t decompressed_file_text;
     if (bytes_vector_init(&decompressed_file_text) != SUCCESS) {
-        //FALTA LIBERAR LA MEMORIA DE compressed_file_string
         return MEMORY_ERROR;
     }
 
-    //asdhakjsdhasdj
+    while (byte_to_read < compressed_file_len) {
+        read_bits = get_char(&compressed_file, &mut byte_to_read, read_bits,
+                &mut decompressed_file_text);
+        if (byte_to_read == (compressed_file_len - 1)) {
+            last_byte_read_bits += read_bits;
+            if ((BITS_PER_BYTE - last_byte_read_bits) == padding_bits) {
+                byte_to_read += 1;
+            }
+        }
+    }
 
-    //Pasar a una funcion que lo libera para que el programa quede simetrico
     if (bytes_vector_release(&decompressed_file_text) != SUCCESS) {
-        //FALTA LIBERAR LA MEMORIA DE compressed_file_string
         return MEMORY_ERROR;
     }
 
@@ -95,31 +138,22 @@ int decompress_file(const char* file_name) {
 
     huffman_tree_decode();
     char* compressed_file_string;
-    int program_status = load_file(file_name, &compressed_file_string);
+    int file_len = 0;
+    int program_status = load_file(file_name, &compressed_file_string, &file_len);
     if (program_status != SUCCESS) {
+        free(compressed_file_string);
         return program_status;
     }
 
+    //llamar a execute_decompression
+    program_status = execute_decompression(file_name, compressed_file_string, file_len);
 
 
+    //Pasar a una funcion que lo libera para que el programa quede simetrico
     free(compressed_file_string);
 
-    let compressed_file = fs::read(file_name).expect("Inexistent file");
-    let padding_bits = compressed_file[0];
-    let mut byte_to_read = 1;
-    let mut read_bits = 0;
-    let mut decompressed_file_text;
-    let mut last_byte_read_bits = 0;
-    while byte_to_read < compressed_file.len() {
-        read_bits = get_char(&compressed_file, &mut byte_to_read, read_bits,
-                &mut decompressed_file_text);
-        if byte_to_read == (compressed_file.len() - 1) {
-            last_byte_read_bits += read_bits;
-            if (BITS_PER_BYTE - last_byte_read_bits) == padding_bits {
-                byte_to_read += 1;
-            }
-        }
-    }
+
+
 
     //CAMBIAR ESTO PORQUE PUEDO HACER QUE LA FUNCION QUE CHEQUEA EL NOMBRE DEL ARCHIVO RETORNE LA POSICION
     //DE LA EXTENSION, TENDRIA QUE SACAR EL EXPECT DEL find
