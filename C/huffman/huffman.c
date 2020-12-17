451d7c41520594e9c789ae5ce060a3c1554035d2
Agustin <aguscambiano@gmail.com>
2020-12-17 18:26:12 -0300

    Cambio huffman_compression para que se pueda guardar en el stack

--- C/huffman/huffman.c
+++ C/huffman/huffman.c
@@ -2,10 +2,16 @@
 #include "huffman.h"
 #include "huffman_tree.h"
 
+#define SUCCESS 0
+#define MEMORY_ERROR -1
+
+/*
 struct huffman_compression {
     huffman_tree_t *tree;
 };
+*/
 
+/*
 huffman_compression_t *huffman_compression_create() {
     huffman_compression_t *huffman = malloc(sizeof(huffman_compression_t));
     if (huffman == NULL) {
@@ -26,7 +32,20 @@ void huffman_compression_destroy(huffman_compression_t *huffman) {
     huffman_tree_destroy(huffman->tree);
     free(huffman);
 }
+*/
+
+int huffman_compression_create(huffman_compression_t *huffman) {
+    if (huffman_tree_init(&huffman->tree) != SUCCESS) {
+        huffman_tree_release(&huffman->tree);
+        return MEMORY_ERROR;
+    }
+    return SUCCESS;
+}
+
+void huffman_compression_destroy(huffman_compression_t *huffman) {
+    huffman_tree_release(&huffman->tree);
+}
 
 void huffman_compression_print_tree(huffman_compression_t *huffman) {
-    huffman_tree_print(huffman->tree);
+    huffman_tree_print(&huffman->tree);
 }
--- C/huffman/huffman.h
+++ C/huffman/huffman.h
@@ -1,14 +1,22 @@
 #ifndef C_HUFFMAN_H
 #define C_HUFFMAN_H
 
-struct huffman_compression;
+#include "huffman_tree.h"
 
-typedef struct huffman_compression huffman_compression_t;
+//struct huffman_compression;
+//typedef struct huffman_compression huffman_compression_t;
 
+typedef struct huffman_compression {
+    huffman_tree_t tree;
+}huffman_compression_t;
 
-huffman_compression_t *huffman_compression_create();
+//huffman_compression_t *huffman_compression_create();
 
-void huffman_compression_destroy(huffman_compression_t *huffman);
+//void huffman_compression_destroy(huffman_compression_t *huffman);
+
+int huffman_compression_init(huffman_compression_t *huffman);
+
+void huffman_compression_release(huffman_compression_t *huffman);
 
 void huffman_compression_print_tree(huffman_compression_t *huffman);
 
