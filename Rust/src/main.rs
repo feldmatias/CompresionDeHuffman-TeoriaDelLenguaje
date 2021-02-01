e218ef326c1c99f801cccded68db2a76c41e9190
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-02-01 13:28:08 -0300

    Agrego mensajes de error claros en la version de Rust

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -14,26 +14,30 @@ const PRINT_TREE_ARGS_AMOUNT: usize = 2;
 const DECOMPRESS_ARG: &str = "decompress";
 const COMPRESS_ARG: &str = "compress";
 const PRINT_TREE: &str = "print";
+const INCORRECT_ARGUMENTS_MESSAGE: &str = "Incorrect Argument: valid arguments are \n\
+                                           1) print [see the tree nodes and char frequencies]\n\
+                                           2) compress filename [compress the file named filename]\n\
+                                           3) decompress filename [decompress the file named filename]\n";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() == PRINT_TREE_ARGS_AMOUNT {
-        if args[PROGRAM_MODE_INDEX] == PRINT_TREE{
+        if args[PROGRAM_MODE_INDEX] == PRINT_TREE {
             let huffman = HuffmanCompression::new();
             huffman.print_tree();
         } else {
-            panic!("Incorrect Argument");
+            panic!("{}", INCORRECT_ARGUMENTS_MESSAGE);
         }
-    } else if args.len() == ARGS_AMOUNT{
+    } else if args.len() == ARGS_AMOUNT {
         if args[PROGRAM_MODE_INDEX] == COMPRESS_ARG {
             compression::compress_file(&args[FILE_INDEX]);
         } else if args[PROGRAM_MODE_INDEX] == DECOMPRESS_ARG {
             decompression::decompress_file(&args[FILE_INDEX]);
         } else {
-            panic!("Incorrect argument");
+            panic!("{}", INCORRECT_ARGUMENTS_MESSAGE);
         }
     } else {
-        println!("Compilado con exito")
+        println!("{}", INCORRECT_ARGUMENTS_MESSAGE);
     }
 }
